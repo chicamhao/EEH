ed2d5740c7036356414fa2f057d77e408ea68f4f
chicamhao <bluevampire0103@gmail.com>
2025-03-17 22:26:33 +0700

    add default generated files

---
+++ UntitledG/Source/UntitledG/UntitledG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UntitledG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UntitledG, "UntitledG" );
