77da790581bbb592e84f2bbcd213852ef4e7236c
chicamhao <bluevampire0103@gmail.com>
2025-03-23 18:33:07 +0700

    implement character movement

---
+++ UntitledG/Source/UntitledG/Public/Characters/ChamuCharacter.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ChamuCharacter.generated.h"
+
+UCLASS()
+class UNTITLEDG_API AChamuCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AChamuCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void MoveForward(const float Value);
+	void MoveRight(const float Value);
+	void Turn(const float Value);
+	void LookUp(const float Value);
+};
