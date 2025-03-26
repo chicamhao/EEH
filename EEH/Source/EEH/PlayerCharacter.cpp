89e0ff8f003353f3598fa710cee6f31cf0e80c84
chicamhao <bluevampire0103@gmail.com>
2025-03-27 00:26:07 +0700

    port character movement

---
+++ EEH/Source/EEH/PlayerCharacter.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(FName("Forward"), this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(FName("Right"), this, &APlayerCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(FName("Turn"), this, &APlayerCharacter::Turn);
+	PlayerInputComponent->BindAxis(FName("LookUp"), this, &APlayerCharacter::LookUp);
+}
+
+void APlayerCharacter::MoveForward(const float Value)
+{
+	if (Controller && Value != 0.0f)
+	{
+		AddMovementInput(GetActorForwardVector(), Value);
+	}	
+}
+
+void APlayerCharacter::MoveRight(const float Value)
+{
+	if (Controller && Value != 0.0f)
+	{
+		AddMovementInput(GetActorRightVector(), Value);
+	}	
+}
+
+void APlayerCharacter::LookUp(const float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+void APlayerCharacter::Turn(const float Value)
+{
+	AddControllerYawInput(Value);
+}
